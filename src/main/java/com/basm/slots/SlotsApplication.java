07046e27f7a605339a54ce60e677108753f73610
basmoorkens <bas_moorkens@hotmail.com>
2018-06-01 23:50:49 +0200

    added swagger ui

--- src/main/java/com/basm/slots/SlotsApplication.java
+++ src/main/java/com/basm/slots/SlotsApplication.java
@@ -1,10 +1,12 @@
 package com.basm.slots;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.basm.slots"})
 @EnableScheduling
