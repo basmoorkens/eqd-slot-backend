3a72251a87b51fd59fc9c2b717d2f3815010b2ae
Ubuntu <ubuntu@ip-172-31-43-43.ec2.internal>
2018-10-30 22:03:02 +0000

    update build and run script

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-cp ./prod_cfg/application.properties ./src/main/resources/application.properties
+cp ../production/spin-to-win-backend.properties ./src/main/resources/application.properties
 
 mvn clean install
 sudo docker build . -t eqd-slots
--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,3 @@
-sudo docker stop eqd-slots-backend
-sudo docker rm eqd-slots-backend
-sudo docker run --name="eqd-slots-backend" -p 8080:8080 -d eqd-slots
-
+docker stop eqd-slots-backend
+docker rm eqd-slots-backend
+docker run -v /var/log/eqd:/var/log --name="eqd-slots-backend" -p 8080:8080 -d eqd-slots
